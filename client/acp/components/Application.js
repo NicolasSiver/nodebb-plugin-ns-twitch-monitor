6c3606984a25dc24d3c636d37528662789149e50
Nicolas Siver <nicolas@siver.im>
2015-06-20 22:01:21 +0300

    naming

---
+++ client/acp/components/Application.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Nicolas on 6/20/15.
+ */
+import React from 'react';
+import Settings from './Settings';
+
+export default class Application extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col-md-7">
+
+                </div>
+                <div className="col-md-5">
+                    <Settings />
+                </div>
+            </div>
+        );
+    }
+}
