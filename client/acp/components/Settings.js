5d6d11982489b528dcb8a7b4a2b97ab08d253879
Nicolas Siver <nicolas@siver.im>
2015-06-21 10:58:45 +0300

    client id input

--- client/acp/components/Settings.js
+++ client/acp/components/Settings.js
@@ -4,16 +4,20 @@
 import React from 'react';
 
 export default class Settings extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
-    render(){
+    render() {
         return (
             <div className="panel panel-default">
                 <div className="panel-heading">Settings</div>
                 <div className="panel-body">
-
+                    <div className="form-group">
+                        <label htmlFor="clientId">Client ID</label>
+                        <input type="text" className="form-control" id="clientId" placeholder="Twitch Client ID"/>
+                        <small>Hint: you should <a href="http://www.twitch.tv/kraken/oauth2/clients/new" target="_blank">register Twitch Application</a> to get client id.</small>
+                    </div>
                 </div>
             </div>
         );
