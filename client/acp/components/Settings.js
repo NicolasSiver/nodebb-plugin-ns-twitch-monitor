e72f9d51f2e749984dfb19f785a86246cb3ed31b
Nicolas Siver <nicolas@siver.im>
2015-06-21 11:24:31 +0300

    client id input with a feedback

--- client/acp/components/Settings.js
+++ client/acp/components/Settings.js
@@ -2,10 +2,38 @@
  * Created by Nicolas on 6/20/15.
  */
 import React from 'react';
+import classNames from 'classnames';
+
+const FEEDBACK = {
+    NORMAL : 0,
+    SUCCESS: 1,
+    FAILURE: 2
+};
 
 export default class Settings extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {feedback: FEEDBACK.NORMAL};
+    }
+
+    getInputByFeedback(feedback) {
+        const hint = <small>Hint: you should
+            <a href="http://www.twitch.tv/kraken/oauth2/clients/new" target="_blank">register Twitch
+                Application</a> to get client id.
+        </small>;
+        const groupClass = classNames({
+            'form-group' : true,
+            'has-success': feedback === FEEDBACK.SUCCESS,
+            'has-error'  : feedback === FEEDBACK.FAILURE
+        });
+
+        return (
+            <div className={groupClass}>
+                <label className="control-label" htmlFor="clientId">Client ID</label>
+                <input type="text" className="form-control" id="clientId" placeholder="Twitch Client ID"/>
+                {hint}
+            </div>
+        );
     }
 
     render() {
@@ -13,11 +41,7 @@ export default class Settings extends React.Component {
             <div className="panel panel-default">
                 <div className="panel-heading">Settings</div>
                 <div className="panel-body">
-                    <div className="form-group">
-                        <label htmlFor="clientId">Client ID</label>
-                        <input type="text" className="form-control" id="clientId" placeholder="Twitch Client ID"/>
-                        <small>Hint: you should <a href="http://www.twitch.tv/kraken/oauth2/clients/new" target="_blank">register Twitch Application</a> to get client id.</small>
-                    </div>
+                    {this.getInputByFeedback()}
                 </div>
             </div>
         );
